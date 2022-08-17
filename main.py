56da98ecc3be5b18b9fae35c1174b13328c52b4f
Jmarbin <108483439+JmarbinGG@users.noreply.github.com>
2022-08-17 01:33:06 +0000

    omg more stuff got updated!!!!!!!!!!!!!!!!!!!!!!!!

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pydealer
 import sys
 import time
+from pydealer.const import POKER_RANKS
+from blessed import Terminal
+t = Terminal()
 
 print("Console Texas Holdem!")
 time.sleep(1)
@@ -40,9 +43,10 @@ p1cards = deck.deal(2)
 time.sleep(1)
 print("The dealer deals the cards")
 hand.add(p1cards)
+hand.sort()
 time.sleep(1)
 print("Your cards are:")
-print(hand)
+print(t.blue(str(hand)))
 time.sleep(1)
 first_bet = input("How much would you like to bet? ")
 first_bet = int(first_bet)
@@ -58,28 +62,39 @@ time.sleep(1)
 print("The dealer burns the top card")
 addFlop = deck.deal(3)
 flop.add(addFlop)
+flop.sort()
 time.sleep(1)
 print("The dealer lays out the flop")
 time.sleep(1)
 print("The cards in the flop are:")
-print(flop)
+print(t.green(str(flop)))
 time.sleep(1)
-second_bet = input("How much would you like to bet? ")
+second_bet = input("How much would you like to bet(or fold)? ")
+if second_bet.equals("fold"):
+  print("You fold")
+  sys.exit()
+
 second_bet = int(second_bet)
 chips -= second_bet
 if chips < second_bet:
   print("ur broke noob")
   sys.exit()
+
 time.sleep(1)
 print("You bet", second_bet, "poker chips, and have", chips, "poker chips left")
 time.sleep(1)
 turn = deck.deal(1)
 flop.add(turn)
+flop.sort()
 time.sleep(1)
 print("The dealer lays out the turn, the flop is now:")
-print(flop)
+print(t.green(str(flop)))
 time.sleep(1)
 third_bet = input("How much would you like to bet? ")
+if third_bet.contains("fold"):
+  print("You fold")
+  sys.exit()
+
 third_bet = int(third_bet)
 chips -= third_bet
 if chips < third_bet:
@@ -90,9 +105,10 @@ print("You bet", third_bet, "poker chips, and have", chips, "poker chips left")
 time.sleep(1)
 river = deck.deal(1)
 flop.add(river)
+flop.sort()
 time.sleep(1)
 print("The dealer lays out the river card, the flop is now:")
-print(flop)
+print(t.green(str(str)))
 time.sleep(1)
 four_bet = input("How much would you like to bet? ")
 four_bet = int(four_bet)
